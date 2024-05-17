5d8a737dc19018a293e78e21ef02b34f9d9558cf
lemoonchild <ncastrom21@gmail.com>
2024-05-16 23:00:10 -0600

    fix: fixed an error in display

--- calculator-project/src/components/display/index.jsx
+++ calculator-project/src/components/display/index.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import '../styles/display.css'
 
 const Display = ({ value }) => {
-  return <div className="display">{value}</div>
+  return (
+    <div className="display" data-testid="display-value">
+      {value}
+    </div>
+  )
 }
 
 export default Display
