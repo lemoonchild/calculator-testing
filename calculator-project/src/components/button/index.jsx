65950edf816bb694a106e38580225289d61e47fc
lemoonchild <ncastrom21@gmail.com>
2024-05-18 16:52:58 -0600

    fix: fixed the main component

--- calculator-project/src/components/button/index.jsx
+++ calculator-project/src/components/button/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import '../styles/numberButton.css'
 
-const Button = ({ label, onClick, isActive }) => {
+const Button = ({ label, isActive, onClick, color, textColor, className }) => {
   return (
-    <button className={`button ${isActive ? 'active' : ''}`} onClick={onClick}>
+    <button
+      className={`button ${isActive ? 'active' : ''} ${className}`}
+      onClick={onClick}
+      style={{ backgroundColor: color, color: textColor }}
+    >
       {label}
     </button>
   )
